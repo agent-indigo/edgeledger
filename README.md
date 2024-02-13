dfb092ec34805607d163aa2c6308278f0db4dacf
agent-indigo <bradenhynes@gmail.com>
2024-02-13 15:00:59 -0330

    Minor improvements

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ## Description
 
-This is the EdgeLedger project from Brad Traversy's 2019 Modern HTML & CSS from the Beginning (Including SASS) course on O'Reilly. I migrated the smooth scrolling behavior from jQuery to CSS.
+This is the EdgeLedger project from Brad Traversy's 2019 Modern HTML & CSS from the Beginning (Including SASS) course on O'Reilly.
+
+I made the following modifications:
+
+- Migrated the smooth scrolling from jQuery to CSS
+
+- Disabled the Google Maps and other JavaScript functionality
--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 // initialize and add the map
-initMap: () => {
+const App = () => {
     // company HQ
-    const loc = { lat: 42.361145, lng: -71.057083 };
+    const loc = { lat: 42.361145, lng: -71.057083 }
     // map centered on company HQ
     const map = new google.maps.Map(document.querySelector('.map'), {
         zoom: 14,
         center: loc
-    });
+    })
     // marker on company HQ
-    const marker = new google.maps.Marker({ position: loc, map: map });
+    const marker = new google.maps.Marker({ position: loc, map: map })
+    // Sticky menu background
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 150) {
+          document.querySelector('#navbar').style.opacity = 0.9
+        } else {
+          document.querySelector('#navbar').style.opacity = 1
+        }
+    })
 }
+export default App
